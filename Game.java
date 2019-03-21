02823b07770ed7d2883a855d03ccdc0cad8bdf07
Sa Cux <fuoridallarete@users.noreply.github.com>
2019-03-21 20:37:50 +0100

    loop fixed
    for loop for updating title fixed.
    still to do:
    check user hasWon; stop the game; play again

--- Game.java
+++ Game.java
@@ -6,9 +6,9 @@ public class Game {
   private Scanner scanGuess;
   //STRING
   private String movieTitle;
-  String wrongGuesses;
-  String rightGuesses;
-  String totalGuessed;
+  private String wrongGuesses = "";
+  private String rightGuesses = "";
+  private String totalGuessed = "";
   //CHAR
   private char guess;
   //ARRAYS
@@ -16,17 +16,19 @@ public class Game {
   private char[] charMovie;
   private char[] hiddenTitle;
   private char[] updateTitle;
+  private char[] charWrongGuesses;
+  private char[] charTotalGuesses;
   //INT
-  private int contaTentativo = 10;
+  private int contaTentativo;
   private int score;
   private int spaces = 0;
   private int movieLength; //movieTitleLength - spaces;
   private int randomNumber = 1; // Random movie number
   //BOOLEAN
-  //private boolean[] isEntered = new boolean[movieTitle.length()];
   private boolean isEntered = false;
   private boolean isGuessed = false;
-  //boolean hasWon;
+  private boolean isDouble = false;
+  //private boolean hasWon;
 
   //costruttore
   Game() {
@@ -43,7 +45,6 @@ public class Game {
     System.out.print("ENTER LETTER: ");
     movieLength = movieTitle.length() - spaces; // without the spacing
     readGuess();
-    //updateTitle();
   }
 
   private int randomMovieNumber() {
@@ -70,7 +71,7 @@ public class Game {
     return arrayFilm.get(randomMovieNumber());
   }
 
-  char[] hideTitle() {
+  public char[] hideTitle() {
     charMovie = movieTitle.toCharArray();
     for (int i = 0; i < movieTitle.length(); i++) {
       if (charMovie[i] != ' ') {
@@ -80,7 +81,8 @@ public class Game {
     return charMovie;
   }
 
-  char readGuess() {
+  public char readGuess() throws AssertionError {
+    isEntered = false;
     scanGuess = null;
     try {
       scanGuess = new Scanner(System.in);
@@ -88,44 +90,67 @@ public class Game {
       e.printStackTrace();
     }
     if (scanGuess == null) throw new AssertionError();
-    do {
-      guess = scanGuess.nextLine().charAt(0);
-      //System.out.println(guess);
-      contaTentativo--;
-      isEntered = true;
-      guess = Character.toUpperCase(guess);
-      updateTitle();
-      scanGuess.close();
-      return guess;
-    } while (contaTentativo < 10 || movieLength > 0);
+
+    guess = scanGuess.nextLine().charAt(0);
+    isEntered = true;
+    guess = Character.toUpperCase(guess);
+    updateTitle();
+    scanGuess.close();
+    return guess;
   }
+
   private void updateTitle() {
     charMovieTitle = movieTitle.toCharArray();
     updateTitle = new char[charMovieTitle.length];
     updateTitle = charMovieTitle;
-    for (int i = 0; i < movieLength; i++) {
-      if (updateTitle[i] == guess) {
-        hiddenTitle[i] = guess;
-        isGuessed = true;
+    isGuessed = false;
+    if(isEntered) {
+      for (int i = 0; i < movieLength; i++) {
+        if (guess == charMovieTitle[i]) {
+          hiddenTitle[i] = guess;
+          isGuessed = true;
+        }
+      }
+      updateTitle = hiddenTitle;
+      options(guess);
+    }
+    do{
+      initMatch();
+      isGuessed = false;
+    } while(contaTentativo != 0 && movieLength > 0);
+  }
+  private boolean checkIfDouble(char guessChar) {
+    totalGuessed = rightGuesses + wrongGuesses;
+    charTotalGuesses = totalGuessed.toCharArray();
+    isDouble = false;
+    for (int i = 0; i < charTotalGuesses.length; i++) {
+      if (guessChar == charTotalGuesses[i]) {
+        isDouble = true;
       }
     }
-
-    updateTitle = hiddenTitle;
-    options();
+    return isDouble;
   }
-  private void options(){
-    //System.out.println(updateTitle);
-    //System.out.println("already entered: " + totalGuessed);
 
-    if(isGuessed) {
-      score++;
-      System.out.println(updateTitle);
-    } else {
-      wrongGuesses += guess;
-      System.out.println(wrongGuesses);
+  private void options(char myGuess){
+    if(checkIfDouble(myGuess)){
+      System.out.println("You already tried this letter. Try another one.");
     }
-    System.out.println("letters already guessed: " + guess);
+    if(!isGuessed){
+      contaTentativo--;
+      wrongGuesses += myGuess;
+    }else {
+      score++;
+      rightGuesses += myGuess;
+    }
+    gameState();
+  }
+
+  private void gameState(){
+    System.out.println(updateTitle);
+    System.out.println("right guesses: " + rightGuesses);
+    System.out.println("wrong guesses: " + wrongGuesses);
+    System.out.println("letters already guessed: " + totalGuessed);
     System.out.println("SCORE: " + score);
-    System.out.println("tentativi rimasti: " + contaTentativo);
+    System.out.println("tries left: " + contaTentativo);
   }
 }
